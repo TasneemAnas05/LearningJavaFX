a440d3d752647dd63000e49125b551c18c5b42bc
TasneemAnas05 <125818114+TasneemAnas05@users.noreply.github.com>
2024-05-21 00:33:57 +0300

    Add files via upload

---
+++ EventsDemoProj/EventsDemoProj/src/eventsdemoproj/EventsDemoProj.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package eventsdemoproj;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ASU
+ */
+public class EventsDemoProj extends Application 
+        implements EventHandler<ActionEvent>{
+    
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        btn1= new Button("Button 1");
+        btn2= new Button("Button 2");
+        btn3= new Button("Button 3");
+        btn4= new Button("Button 4");
+        btn5= new Button("Button 5");
+        btn6= new Button("Button 6");
+        btn7= new Button("Button 7");
+        btn8= new Button("Button 8");
+        
+        
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Anonymous class event handler");
+            }
+        });
+        
+        btn2.setOnAction(e->{
+            System.out.println("Lambda expression event handler");
+        });
+        
+        btn3.setOnAction(e-> printMessage());
+        
+        MyHandler myHandler= new MyHandler();
+        btn4.setOnAction(myHandler);
+        btn5.setOnAction(myHandler);
+        
+        btn6.setOnAction(this);
+        
+        btn7.addEventHandler(
+                MouseEvent.MOUSE_CLICKED,
+                e-> {
+                    System.out.println ("ADD EVENT HANDLER");
+                }
+            );
+        
+        btn8.addEventFilter(MouseEvent.MOUSE_CLICKED, 
+                e->{System.out.println("btn8 event filter");});
+        
+        btn7.addEventFilter(MouseEvent.MOUSE_CLICKED, 
+                e->{System.out.println("btn7 event filter");});
+        
+        
+        FlowPane root = new FlowPane();
+        root.getChildren().addAll(btn1,btn2, btn3, btn4, 
+                btn5, btn6, btn7, btn8);
+        
+        
+        root.addEventFilter(MouseEvent.MOUSE_CLICKED, 
+                e->{
+                    System.out.println("ROOT event filter");
+                    e.consume();//to use only one then remove the event
+                });
+        
+        primaryStage.addEventFilter(MouseEvent.MOUSE_CLICKED, 
+                e->{
+                    System.out.println("Stage event filter");
+                    
+                });
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    void printMessage(){
+        System.out.println("Lambda expression method call");
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        System.out.println("Application class event handler");
+    }
+    
+    
+    
+    class MyHandler implements EventHandler<ActionEvent>{
+        @Override
+        public void handle(ActionEvent event) {
+            if(event.getSource()==btn4){
+                System.out.println("Inner class event handler from BTN 4");
+            }else{
+                System.out.println("Inner class event handler from BTN 5");
+            }
+        }
+    }
+    
+}

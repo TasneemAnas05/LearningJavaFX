e4db428bf039934f7b77c13c2334b49ded822c49
TasneemAnas05 <125818114+TasneemAnas05@users.noreply.github.com>
2024-05-20 22:50:52 +0300

    Update MenuProj.java

--- MenuProj/src/menuproj/MenuProj.java
+++ MenuProj/src/menuproj/MenuProj.java
@@ -8,12 +8,14 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -21,99 +23,111 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author lab1g11
+ * @author ASU
  */
 public class MenuProj extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-
+        
+        
         BorderPane root = new BorderPane();
-        MenuBar menuBar = buildMenu();
+        
+        MenuBar menuBar= buildMenu();
         root.setTop(menuBar);
-        Button btn = new Button("_dialog");
-        btn.setMnemonicParsing(true);
+        
+        Button btn= new Button("_dialog");
+        btn.setMnemonicParsing(true);//so we can 
         btn.setOnAction(new EventHandler<ActionEvent>() {
-            
             @Override
             public void handle(ActionEvent event) {
                 JOptionPane.showMessageDialog(null, "Test message");
             }
         });
+        
         root.setCenter(btn);
         btn.setContextMenu(buildContextMenu());
-       
+        
+        
         Scene scene = new Scene(root, 300, 250);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    private MenuBar buildMenu() {
-        MenuItem newProjItem = new MenuItem("New Project");
-        MenuItem exitItem = new MenuItem("_Exit");
-        exitItem.setMnemonicParsing(true);//getback in here
+    
+    private MenuBar buildMenu(){
         
-        Menu fileMenu = new Menu("_File", null, newProjItem, exitItem);//add items to it 
+        MenuItem newProjItem= new MenuItem("New project");
+        MenuItem exitItem= new MenuItem("_Exit");
+        exitItem.setMnemonicParsing(true);
+        
+        Menu fileMenu= new Menu("_File", null, newProjItem, exitItem);
         fileMenu.setMnemonicParsing(true);
-        MenuItem undoItem = new MenuItem("Undo");
-        MenuItem redoItem = new MenuItem("Redo");
         
-        Menu editMenu = new Menu("Edit");//or add them using a method 
-        editMenu.getItems().addAll(undoItem , redoItem);
-
-        MenuBar menuBar = new MenuBar(fileMenu);
+        MenuItem undoItem= new MenuItem("Undo");
+        MenuItem redoItem= new MenuItem("Redo");
+        
+        Menu editMenu= new Menu("Edit");
+        editMenu.getItems().addAll(undoItem, redoItem);
+        
+        MenuBar menuBar= new MenuBar(fileMenu);
         menuBar.getMenus().add(editMenu);
         
         exitItem.setOnAction(new EventHandler<ActionEvent>() {
-            
             @Override
             public void handle(ActionEvent event) {
-                System.exit(0);//to exit the application 
+                System.exit(0);
             }
         });
+        
         fileMenu.setOnShowing(new EventHandler<Event>() {
             @Override
             public void handle(Event event) {
-                System.out.println("file menu showing");
-                
+                System.err.println("file menue showing");
             }
         });
+        
         fileMenu.setOnShown(new EventHandler<Event>() {
             @Override
             public void handle(Event event) {
-                System.out.println("file menu shown");
+                System.err.println("file menue shown");
             }
         });
+        
         fileMenu.setOnHiding(new EventHandler<Event>() {
             @Override
             public void handle(Event event) {
-                System.err.println("file menu hiding");
+                System.err.println("file menue hiding");
             }
         });
+        
         fileMenu.setOnHidden(new EventHandler<Event>() {
             @Override
             public void handle(Event event) {
-                System.err.println("file menu hidden");
+                System.err.println("file menue hidden");
             }
         });
+        
         return menuBar;
+        
     }
-   private ContextMenu buildContextMenu(){
-       MenuItem copyItem=new MenuItem("Copy");
-       MenuItem cutItem=new MenuItem("Cut");
-       MenuItem pasteItem=new MenuItem("Paste");
-       cutItem.setOnAction(new EventHandler<ActionEvent>() {
-            
+
+    private ContextMenu buildContextMenu(){
+        
+        MenuItem copyItem= new MenuItem("Copy");
+        MenuItem cutItem= new MenuItem("Cut");
+        MenuItem pasteItem= new MenuItem("Paste");
+        
+        cutItem.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 JOptionPane.showMessageDialog(null, "Cut item clicked");
             }
         });
-       ContextMenu ctxMenu=new ContextMenu(copyItem , cutItem , pasteItem);
-       return ctxMenu;
-   }
+        ContextMenu ctxMenu= new ContextMenu(copyItem, cutItem, pasteItem);
+        return ctxMenu;
+    }
     /**
      * @param args the command line arguments
      */
